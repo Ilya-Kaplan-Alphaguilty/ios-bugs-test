5538411f5748107fe24cfd30190627ef98714b88
Ilya Kaplan <ik@alphaguilty.io>
2023-11-22 13:36:59 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const inputRef = React.useRef(null);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +20,55 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1>Without on touch start</h1>
+        <input type="text" name="test" id="test" />
+        <button onClick={() => alert("Clicked")}>test</button>
+
+        <h1>With on touch start</h1>
+        <input
+          type="text"
+          name="test1"
+          id="test1"
+          onTouchStart={(e) => {
+            if (
+              !(
+                navigator?.userAgent?.toLowerCase()?.includes("safari") &&
+                navigator?.userAgent?.toLowerCase()?.includes("iphone")
+              )
+            ) {
+              return;
+            }
+
+            inputRef.current.focus();
+          }}
+        />
+        <button
+          onClick={(e) => {
+            if (
+              navigator?.userAgent?.toLowerCase()?.includes("safari") &&
+              navigator?.userAgent?.toLowerCase()?.includes("iphone")
+            ) {
+              return;
+            }
+
+            alert("Clicked");
+          }}
+          onTouchStart={(e) => {
+            if (
+              !(
+                navigator?.userAgent?.toLowerCase()?.includes("safari") &&
+                navigator?.userAgent?.toLowerCase()?.includes("iphone")
+              )
+            ) {
+              return;
+            }
+
+            alert("Clicked");
+          }}
+        >
+          test
+        </button>
       </header>
     </div>
   );
